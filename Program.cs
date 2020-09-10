cda455ee3d623ac45cf5d689c0ce58f37a9f7bdc
Artёm Skvira <art@nimblegecko.com>
2020-09-10 15:02:01 +1000

    Project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_net_core_encrypted_local_config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
