e4476047298800e80d80b084ea3cd958d33ea645
Artёm Skvira <art@nimblegecko.com>
2020-09-11 13:06:28 +1000

    Added references

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
 
 namespace dot_net_core_encrypted_local_config
 {
@@ -6,6 +8,7 @@ namespace dot_net_core_encrypted_local_config
     {
         static void Main(string[] args)
         {
+            var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
             Console.WriteLine("Hello World!");
         }
     }
